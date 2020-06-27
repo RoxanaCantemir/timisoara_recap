b527755a0d93c943c79b6b066d83557d1345d255
RoxanaCantemir <roxana_c1991@yahoo.com>
2020-06-27 11:19:37 +0300

    modificat main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
         Stock stock = new Stock();
-        stock.setName("Parfum");
+        stock.setName("Perfume");
         stock.setValue(5);
         Session session = HibernateConfig.getSession();
         session.beginTransaction();
